8e4ba04c059d10eb5f0df9e6f9a7f786200d2d2f
malekbena <malekb.benali@gmail.com>
2023-10-06 11:34:44 +0200

    gallery filter

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,21 +1,79 @@
 const api_url = "http://localhost:5678/api/"
 
+
+let works = new Set()
+let selectedWorks = new Set()
+let selectedCat = 0
+
+const gallery = document.querySelector(".gallery")
+const categories = document.querySelector(".categories")
+
+initWorks()
+displayCatsBtn()
+
 async function getWorks(api_url) {
     const url = api_url + "works"
     const response = await fetch(url)
     var data = await response.json()
+    data.forEach(element => {
+        works.add(element)
+    });
     return data
 }
-displayWorks()
 
-const gallery = document.querySelector(".gallery")
+async function getCats(api_url) {
+    const url = api_url + "categories"
+    const response = await fetch(url)
+    var data = await response.json()
+    return data
+}
 
-async function displayWorks() {
-    const works = await getWorks(api_url)
-    works.forEach(work => {
-        gallery.innerHTML += `<figure>
-        <img src="${work.imageUrl}" alt="${work.title}">
-        <figcaption>${work.title}</figcaption>
-    </figure>`
+async function initWorks() {
+    const data = await getWorks(api_url)
+    data.forEach(work => {
+        works.add(work)
     })
+    displayWorks(selectedCat)
+}
+
+function displayWorks(selectedCat) {
+    selectedWorks.clear()
+    if (selectedCat == 0) {
+        works.forEach(work => {
+            selectedWorks.add(work)
+        })
+    } else {
+        works.forEach(work => {
+            if (work.categoryId == selectedCat) {
+                selectedWorks.add(work)
+            }
+        })
+    }
+        selectedWorks.forEach(work => {
+            gallery.innerHTML += `<figure>
+                        <img src="${work.imageUrl}" alt="${work.title}">
+                        <figcaption>${work.title}</figcaption>
+                        </figure>`
+        })
+}
+
+async function displayCatsBtn() {
+    const cats = await getCats(api_url)
+    categories.innerHTML = `<button class='cats-btn ${selectedCat == 0 ? "cats-btn-selected" : ""}' id='0'>Tous</button>`
+    cats.forEach(cat => {
+        categories.innerHTML += `<button class="cats-btn ${selectedCat == cat.id ? "cats-btn-selected" : ""} " id="${cat.id}">${cat.name}</button>`
+    })
+    const catsBtn = document.querySelectorAll(".cats-btn")
+    catsBtn.forEach(catBtn => {
+        catBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            changeCat(catBtn.id)
+        })
+    })
+}
+function changeCat(catId) {
+    selectedCat = catId
+    gallery.innerHTML = ""
+    displayWorks(selectedCat)
+    displayCatsBtn()
 }
